2b046ed02cdbae83d22b263282f0fb0f7b434269
Ofosua <maryas182@gmail.com>
2023-08-07 18:20:07 -0430

    The last digit

---
+++ python-hello_world/3-last_digit.py
@@ -0,0 +1,15 @@
+import random
+number = random.randint(-10000, 10000)
+
+if number >= 0:
+    answer = number % 10
+    if answer > 5:
+        print("Last digit of {} is {} and is greater than 5".format(number,answer))
+    elif answer == 0:
+        print("Last digit of {} is {} and is 0".format(number,answer))
+    elif answer < 6 and answer != 0:
+        print("Last digit of {} is {} and is less than 6 and not 0".format(number,answer))
+
+else:
+    answer = (-number) % 10
+    print("Last digit of {} is -{} and is less than 6 and not 0".format(number,answer))
