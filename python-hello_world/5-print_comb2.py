79955141acb4e33155563e81f669376260b60945
Ofosua <maryas182@gmail.com>
2023-08-07 19:46:19 -0430

    modify 00.....99

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -3,11 +3,11 @@ while count <= 9:
     count1 = 0
     while count1 <= 9:
      print("{}{}".format(count,count1), end='')
-     if count != 9 or count1 != 9:
-        print(",",end='')
-     else:
+     if count == 9 and count1 == 9:
+        print(' ')
         break
+     print(",",end='')
+     
      count1 += 1
     count += 1
 
-print(' ')
