43f0d435286ceab50408c52de02a9df315fd1c0b
Ofosua <maryas182@gmail.com>
2023-08-13 00:38:05 -0430

    make a script dynamic!

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,13 @@
+if __name__ == "__main__":
+    from sys import argv
+    l = len(argv)
+    if l == 1:
+        print("0 arguments.")
+    elif l == 2:
+        print("{} argument:".format(l-1))
+        print("1: {}".format(argv[1]))
+    else :
+        a = 1
+        print("{} arguments:".format(l-1))
+        while a < l:
+            print("{}: {} ".format((a),argv[a]))
