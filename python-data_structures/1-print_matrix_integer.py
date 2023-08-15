ef25938488068de3f52e5a569799f093bffd2f57
Ofosua <maryas182@gmail.com>
2023-08-15 11:58:20 -0430

    Lists of lists = Matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -8,18 +8,8 @@ def print_matrix_integer(matrix=[[]]):
     else:
      while c < col:
         while r < row:
-            print("{} ".format(matrix[c][r]), end='')
+            print("{:d} ".format(matrix[c][r]), end='')
             r+=1
         print('')
         r = 0
         c+=1      
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
